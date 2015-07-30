a608a036a2585a6691bb65df68a9a9e2ac00dd9d
fancymax <lindahai_max@126.com>
2015-07-30 18:40:16 +0800

    Initial Commit

---
+++ Train12306/AppDelegate.swift
@@ -0,0 +1,27 @@
+//
+//  AppDelegate.swift
+//  Train12306
+//
+//  Created by fancymax on 15/7/30.
+//  Copyright (c) 2015年 fancy. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+    @IBOutlet weak var window: NSWindow!
+
+
+    func applicationDidFinishLaunching(aNotification: NSNotification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(aNotification: NSNotification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
