23bb8cbc91a0c03fbc6288bb99f5c4fdcc8e2897
fancymax <lindahai_max@126.com>
2016-08-08 17:50:38 +0800

    improve trainfilter

--- 12306ForMac/TicketViewControllers/TrainFilterWindowController.swift
+++ 12306ForMac/TicketViewControllers/TrainFilterWindowController.swift
@@ -32,6 +32,8 @@ class TrainFilterWindowController: NSWindowController,NSTableViewDelegate,NSTabl
     var trainFilterKey = ""
     var seatFilterKey = ""
         
+    @IBOutlet weak var trainFilterTable: NSTableView!
+    
     func createFilterItemByTrains(trains:[QueryLeftNewDTO]){
         filterItems.append(FilterPresentationItem(type: 0,key:"",presentation: "席别类型",isChecked: false))
         filterItems.append(FilterPresentationItem(type: 1,key:"9",presentation: "商务座",isChecked: false))
@@ -44,22 +46,22 @@ class TrainFilterWindowController: NSWindowController,NSTableViewDelegate,NSTabl
         filterItems.append(FilterPresentationItem(type: 1,key:"1",presentation: "硬座",isChecked: true))
         filterItems.append(FilterPresentationItem(type: 1,key:"1|O",presentation: "无座",isChecked: false))
         filterItems.append(FilterPresentationItem(type: 0,key:"",presentation: "出发时段",isChecked: false))
-        filterItems.append(FilterPresentationItem(type: 2,key:"",presentation: "00:00--06:00",isChecked: true))
-        filterItems.append(FilterPresentationItem(type: 2,key:"",presentation: "06:00--12:00",isChecked: true))
-        filterItems.append(FilterPresentationItem(type: 2,key:"",presentation: "12:00--18:00",isChecked: true))
-        filterItems.append(FilterPresentationItem(type: 2,key:"",presentation: "18:00--24:00",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 2,key:"00|06",presentation: "00:00--06:00",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 2,key:"06|12",presentation: "06:00--12:00",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 2,key:"12|18",presentation: "12:00--18:00",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 2,key:"18|24",presentation: "18:00--24:00",isChecked: true))
         filterItems.append(FilterPresentationItem(type: 0,key:"",presentation: "车次类型",isChecked: false))
-        filterItems.append(FilterPresentationItem(type: 3,key:"",presentation: "GC高铁/城际",isChecked: true))
-        filterItems.append(FilterPresentationItem(type: 3,key:"",presentation: "D动车",isChecked: true))
-        filterItems.append(FilterPresentationItem(type: 3,key:"",presentation: "Z字头",isChecked: true))
-        filterItems.append(FilterPresentationItem(type: 3,key:"",presentation: "T字头",isChecked: true))
-        filterItems.append(FilterPresentationItem(type: 3,key:"",presentation: "K字头",isChecked: true))
-        filterItems.append(FilterPresentationItem(type: 3,key:"",presentation: "其它(L/Y)",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 3,key:"G|C",presentation: "GC高铁/城际",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 3,key:"D",presentation: "D动车",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 3,key:"Z",presentation: "Z字头",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 3,key:"T",presentation: "T字头",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 3,key:"K",presentation: "K字头",isChecked: true))
+        filterItems.append(FilterPresentationItem(type: 3,key:"L|Y",presentation: "其它(L/Y)",isChecked: true))
         filterItems.append(FilterPresentationItem(type: 0,key:"",presentation: "指定车次",isChecked: true))
         //demos.append(DemoModel(type: 4,key: "D2683 北京 - 上海 21:23~09:13 历时11:50",isChecked: false))
         
         for train in trains {
-            let key = train.TrainCode!
+            let key = "\(train.TrainCode!)|\(train.start_time!)"
             let presentation = "\(train.TrainCode!) \(train.FromStationName!)->\(train.ToStationName!) \(train.start_time!)~\(train.arrive_time!)"
             filterItems.append(FilterPresentationItem(type: 4, key: key, presentation: presentation, isChecked: true))
         }
@@ -85,9 +87,40 @@ class TrainFilterWindowController: NSWindowController,NSTableViewDelegate,NSTabl
     }
     
     @IBAction func checkTrainFilter(sender: NSButton) {
-//        sender.ob
-        print("check Train Filter")
+        let row = trainFilterTable.rowForView(sender)
+        let item = filterItems[row]
+        
+        var changeState = false
+        if sender.state == NSOnState {
+            changeState = true
+        }
+        else {
+            changeState = false
+        }
+        
+        if item.type == 2 {
+            let filterKeys = item.key.componentsSeparatedByString("|")
+            for item in filterItems where item.type == 4 {
+                let startTime = item.key.componentsSeparatedByString("|")[1]
+                if ((startTime >= filterKeys[0]) && (startTime <= filterKeys[1])) {
+                    item.isChecked = changeState
+                }
+            }
+        }
+        
+        if item.type == 3 {
+            let filterKeys = item.key.componentsSeparatedByString("|")
+            for item in filterItems where item.type == 4 {
+                for filterKey in filterKeys {
+                    if item.key.containsString(filterKey) {
+                        item.isChecked = changeState
+                    }
+                }
+            }
+        }
+        trainFilterTable.reloadData()
     }
+    
     func numberOfRowsInTableView(tableView: NSTableView) -> Int {
         return filterItems.count
     }
