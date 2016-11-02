eb72f8ab2a8dfe4ac099341cbe6f84900fd06bb1
fancymax <lindahai_max@126.com>
2016-11-02 16:58:58 +0800

    Add: 增加学生票预定

--- 12306ForMac/Model/PassengerDTO.swift
+++ 12306ForMac/Model/PassengerDTO.swift
@@ -10,22 +10,18 @@ import Foundation
 
 class PassengerDTO:NSObject {
     
-//    override var description:String{
-//        return "name:\(passenger_name) sex:\(sex_name)"
-//    }
-    
-    var code :String?
-    var passenger_name :String?
+    let code :String
+    let passenger_name :String
     var sex_code :String?
     var sex_name :String?
     var born_date :String?
     var country_code :String?
-    var passenger_id_type_code :String?
+    let passenger_id_type_code :String
     var passenger_id_type_name :String?
-    var passenger_id_no :String?
-    var passenger_type :String?
-    var passenger_flag :String?
+    let passenger_id_no :String
+    let passenger_type :String
     var passenger_type_name :String?
+    var passenger_flag :String?
     var mobile_no :String?
     var phone_no :String?
     var email :String?
@@ -39,24 +35,21 @@ class PassengerDTO:NSObject {
     var isChecked: Bool
     var seatCode = "O";
     var seatCodeName = "二等座";
-    var ticketCode = "1";
-    var ticketCodeName = "成人票";
-    
     
     init(json:JSON)
     {
         isChecked = false
         
-        code = json["code"].string
-        passenger_name = json["passenger_name"].string
+        code = json["code"].string!
+        passenger_name = json["passenger_name"].string!
         sex_code = json["sex_code"].string
         sex_name = json["sex_name"].string
         born_date = json["born_date"].string
         country_code = json["country_code"].string
-        passenger_id_type_code = json["passenger_id_type_code"].string
+        passenger_id_type_code = json["passenger_id_type_code"].string!
         passenger_id_type_name = json["passenger_id_type_name"].string
-        passenger_id_no = json["passenger_id_no"].string
-        passenger_type = json["passenger_type"].string
+        passenger_id_no = json["passenger_id_no"].string!
+        passenger_type = json["passenger_type"].string!
         passenger_flag = json["passenger_flag"].string
         passenger_type_name = json["passenger_type_name"].string
         mobile_no = json["mobile_no"].string
