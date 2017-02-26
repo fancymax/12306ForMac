2db9c2394d934c38c055d604346e764c1ebd2e89
fancymax <lindahai_max@126.com>
2017-02-26 18:06:54 +0800

    improve trainCodeDetailParams

--- 12306ForMac/Model/QueryByTrainCodeParam.swift
+++ 12306ForMac/Model/QueryByTrainCodeParam.swift
@@ -9,20 +9,24 @@
 import Foundation
 
 struct QueryTrainCodeParam {
-    let train_no:String //6i000D232806"
-    let from_station_telecode:String // "IOQ"
-    let to_station_telecode:String //"FYS"
-    let depart_date:String  //2016-09-10 ！！列车出发时间！！
+    let _train_no:String //6i000D232806"
+    let _from_station_telecode:String // "IOQ"
+    let _to_station_telecode:String //"FYS"
+    let _depart_date:String  //2016-09-10 ！！列车出发时间！！
     
     init(_ ticket:QueryLeftNewDTO) {
-        train_no = ticket.train_no
-        from_station_telecode = ticket.FromStationCode!
-        to_station_telecode = ticket.ToStationCode!
-        depart_date = ticket.start_train_date_formatStr!
+        self.init(train_no:ticket.train_no,from_station_telecode:ticket.FromStationCode!,to_station_telecode:ticket.ToStationCode!,depart_date:ticket.start_train_date_formatStr!)
+    }
+    
+    init(train_no:String,from_station_telecode:String,to_station_telecode:String,depart_date:String) {
+        _train_no = train_no
+        _from_station_telecode = from_station_telecode 
+        _to_station_telecode = to_station_telecode 
+        _depart_date = depart_date 
     }
     
     //train_no=6i000D232806&from_station_telecode=IOQ&to_station_telecode=FYS&depart_date=2016-06-12
     func ToGetParams()->String{
-        return "train_no=\(train_no)&from_station_telecode=\(from_station_telecode)&to_station_telecode=\(to_station_telecode)&depart_date=\(depart_date)"
+        return "train_no=\(_train_no)&from_station_telecode=\(_from_station_telecode)&to_station_telecode=\(_to_station_telecode)&depart_date=\(_depart_date)"
     }
 }
--- 12306ForMac/Model/QueryTrainPriceParam.swift
+++ 12306ForMac/Model/QueryTrainPriceParam.swift
@@ -9,23 +9,28 @@
 import Foundation
 
 struct QueryTrainPriceParam {
-    let train_no:String // "6i000D232806"
-    let from_station_no:String // "01"
-    let to_station_no:String // "14"
-    let seat_types:String // "OM9"
-    let train_date:String //  "2016-12-29"
+    let _train_no:String // "6i000D232806"
+    let _from_station_no:String // "01"
+    let _to_station_no:String // "14"
+    let _seat_types:String // "OM9"
+    let _train_date:String //  "2016-12-29"
     
     init(_ ticket:QueryLeftNewDTO) {
-        train_no = ticket.train_no
-        from_station_no = ticket.from_station_no!
-        to_station_no = ticket.to_station_no!
-        seat_types = ticket.seat_types!
-        train_date = ticket.trainDateStr
+        self.init(train_no:ticket.train_no,from_station_no:ticket.from_station_no!,to_station_no:ticket.to_station_no!,seat_types:ticket.seat_types!,train_date:ticket.trainDateStr)
+    }
+    
+
+    init(train_no:String,from_station_no:String,to_station_no:String,seat_types:String,train_date:String) {
+        _train_no = train_no
+        _from_station_no = from_station_no
+        _to_station_no = to_station_no
+        _seat_types = seat_types
+        _train_date = train_date
     }
     
     //?train_no=6i000G160202&from_station_no=01&to_station_no=14&seat_types=OM9&train_date=2016-12-2
     func ToGetParams()->String{
-        return "train_no=\(train_no)&from_station_no=\(from_station_no)&to_station_no=\(to_station_no)&seat_types=\(seat_types)&train_date=\(train_date)"
+        return "train_no=\(_train_no)&from_station_no=\(_from_station_no)&to_station_no=\(_to_station_no)&seat_types=\(_seat_types)&train_date=\(_train_date)"
     }
 }
 
