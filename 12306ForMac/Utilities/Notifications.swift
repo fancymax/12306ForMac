ad1972ef0a56915a1fc4792ea692a9d95e6ec585
fancymax <lindahai_max@126.com>
2017-04-03 21:56:33 +0800

    improve code

---
+++ 12306ForMac/Utilities/Notifications.swift
@@ -0,0 +1,36 @@
+//
+//  Notifications.swift
+//  12306ForMac
+//
+//  Created by fancymax on 2016/11/14.
+//  Copyright © 2016年 fancy. All rights reserved.
+//
+
+import Foundation
+
+extension Notification.Name {
+    public struct App {
+        public static let DidLogin = Notification.Name(rawValue: "com.12306ForMac.App.DidLogin")
+        
+        public static let DidLogout = Notification.Name(rawValue: "com.12306ForMac.App.DidLogout")
+        
+        public static let DidStartQueryTicket = Notification.Name(rawValue: "com.12306ForMac.App.DidStartQueryTicket")
+        
+        public static let DidRefilterQueryTicket = Notification.Name(rawValue: "com.12306ForMac.App.DidRefilterQueryTicket")
+        
+        public static let DidTrainFilterKeyChange = Notification.Name(rawValue: "com.12306ForMac.App.DidTrainFilterKeyChange")
+        
+        public static let DidSubmit = Notification.Name(rawValue: "com.12306ForMac.App.DidSubmit")
+        
+        public static let DidCheckPassenger = Notification.Name(rawValue: "com.12306ForMac.App.DidCheckPassenger")
+        
+        public static let DidAutoLogin = Notification.Name(rawValue: "com.12306ForMac.App.DidAutoLogin")
+        
+        public static let DidAutoSubmit = Notification.Name(rawValue: "com.12306ForMac.App.DidAutoSubmit")
+        public static let DidAutoSubmitWithoutRandCode = Notification.Name(rawValue: "com.12306ForMac.App.DidAutoSubmitWithoutRandCode")
+        
+        public static let DidAddDefaultPassenger = Notification.Name(rawValue: "com.12306ForMac.App.DidAddDefaultPassenger")
+        
+        public static let DidDamaGetBalance = Notification.Name(rawValue: "com.12306ForMac.App.DidDamaGetBalance")
+    }
+}
--- README.md
+++ README.md
@@ -1,22 +1,26 @@
-#12306ForMac
+# 12306ForMac
+
 Mac版12306 订票/捡票 助手。
 
 以前要么开Windows虚拟机，要么使用官方Web，现在可以使用12306ForMac订票助手。
 
 注意系统要求  **OS X10.11**  以上
 
-#下载
+# 下载
+
 [Mac App Store](https://itunes.apple.com/cn/app/ding-piao-zhu-shou/id1163682213?l=zh&ls=1&mt=12)
 
 
 ![demo](screenshot/12306ForMac.jpg)
 
-#开发
+# 开发
+
 1. OS X 10.12/Xcode 8.0/Swift 3.0
 2. $ git clone --recursive https://github.com/fancymax/12306ForMac.git 
 3. $ pod install
 4. open 12306ForMac.xcworkspace
 
-#感谢
+# 感谢
+
 本项目基于 Alamofire、PromiseKit、FMDB、MASPreferences等进行开发，在此表示感谢。
 
