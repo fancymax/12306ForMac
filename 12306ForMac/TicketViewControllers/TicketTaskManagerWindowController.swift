d67b35767c96fecf5c3e2f65d4b64702d6baa458
fancymax <lindahai_max@126.com>
2017-04-13 20:21:02 +0800

    improve ticketTask

--- 12306ForMac/TicketViewControllers/TicketTaskManagerWindowController.swift
+++ 12306ForMac/TicketViewControllers/TicketTaskManagerWindowController.swift
@@ -11,7 +11,7 @@ import Cocoa
 class TicketTask:NSObject {
     var startStation = "北京"
     var endStation = "上海"
-    var date = "2017-01-02"
+    var date = "2017-04-22/2017-04-23/2017-04-24"
     
 }
 
@@ -20,6 +20,12 @@ class TicketTaskManagerWindowController: BaseWindowController {
     override var windowNibName: String{
         return "TicketTaskManagerWindowController"
     }
+    
+    var calendarViewController:LunarCalendarView?
+    
+    @IBOutlet weak var ticketTaskTable: NSTableView!
+    
+    var calendarRow = -1
     
     var ticketTasks = [TicketTask]()
 
@@ -32,11 +38,38 @@ class TicketTaskManagerWindowController: BaseWindowController {
         
     }
     
-    
     @IBAction func clickOK(_ button:NSButton){
         dismissWithModalResponse(NSModalResponseCancel)
     }
     
+    func getDatesByRow(_ row:Int) ->[Date] {
+        var resDates = [Date]()
+        let datesString = ticketTasks[row].date
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        for dateStr in datesString.components(separatedBy: "/") {
+            if let date = dateFormatter.date(from: dateStr) {
+                resDates.append(date)
+            }
+        }
+    
+        return resDates
+    }
+    
+    func convertDates2Str(_ dates:[Date]) -> String {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "yyyy-MM-dd"
+        var resStr = ""
+        for i in 0...dates.count - 1 {
+            let str = dateFormatter.string(from: dates[i])
+            resStr.append(str)
+            if i != dates.count - 1 {
+                resStr.append("/")
+            }
+        }
+        
+        return resStr
+    }
 }
 
 // MARK: - AutoCompleteTableViewDelegate
@@ -84,11 +117,44 @@ extension TicketTaskManagerWindowController: NSTableViewDelegate{
         let view = tableView.make(withIdentifier: tableColumn!.identifier, owner: nil) as! NSTableCellView
         
         let columnIdentifier = tableColumn!.identifier
-        if(columnIdentifier == "AutomaticTableColumnIdentifier.0"){
+        if(columnIdentifier == "startStation" || columnIdentifier == "endStation"){
             let cell = view.viewWithTag(1) as! AutoCompleteTextField
             cell.tableViewDelegate = self
         }
+        else if (columnIdentifier == "date") {
+            let cell = view.viewWithTag(1) as! NSButton
+            cell.target = self
+            cell.action = #selector(showCalendar(_:))
+        }
         
         return view
+    }
+}
+
+// MARK: - NSPopoverDelegate
+extension TicketTaskManagerWindowController:NSPopoverDelegate {
+    
+    @IBAction func showCalendar(_ sender: NSButton){
+        
+        let calendarPopover = NSPopover()
+        let cp = LunarCalendarView(with:Date())
+        calendarRow = self.ticketTaskTable.row(for: sender)
+        cp.allSelectedDates = getDatesByRow(calendarRow)
+        calendarPopover.contentViewController = cp
+        calendarPopover.appearance = NSAppearance(named: "NSAppearanceNameAqua")
+        calendarPopover.animates = true
+        calendarPopover.behavior = NSPopoverBehavior.transient
+        calendarPopover.delegate = self
+        
+        self.calendarViewController = cp
+        let cellRect = sender.bounds
+        calendarPopover.show(relativeTo: cellRect, of: sender, preferredEdge: .maxX)
+    }
+    
+    func popoverDidClose(_ notification: Notification) {
+        let dates = calendarViewController!.allSelectedDates
+        ticketTasks[calendarRow].date = convertDates2Str(dates)
+        
+        ticketTaskTable.reloadData()
     }
 }
